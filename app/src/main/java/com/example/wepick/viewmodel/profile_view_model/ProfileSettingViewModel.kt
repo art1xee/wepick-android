5f2cdb81e5099204749ec9c70622d835c3c25aad
Зубрицький Максим Васильович IПЗ-112К9 <makczub@gmail.com>
2026-09-15 13:47:48 +0300

    FEAT: add ViewModel handlers for privacy/push/email/sound toggles)

--- app/src/main/java/com/example/wepick/viewmodel/profile_view_model/ProfileSettingViewModel.kt
+++ app/src/main/java/com/example/wepick/viewmodel/profile_view_model/ProfileSettingViewModel.kt
@@ -117,6 +117,7 @@ class ProfileSettingViewModel(
         _uiState.update { it.copy(error = null) }
     }
 
+
     fun onPrivacyChanged(value: Boolean) {
         viewModelScope.launch {
             _uiState.update {
@@ -127,14 +128,103 @@ class ProfileSettingViewModel(
             userRepository.setPrivacy(value)
                 .onSuccess {
                     _uiState.update {
-                        it.copy(userProfile = it.userProfile?.copy(isPrivate = value), isLoading = false)
+                        it.copy(
+                            userProfile = it.userProfile?.copy(isPrivate = value),
+                            isLoading = false
+                        )
                     }
-                }.onFailure {e ->
+                }.onFailure { e ->
                     Log.e("SET_PRIVACY_ERROR", "Setting privacy error: ${e.message}", e)
                     _uiState.update {
                         it.copy(
                             error = e.localizedMessage?.let { msg -> UiText.DynamicString(msg) }
-                                ?: UiText.DynamicString("Set Privacy status error"), isLoading = false
+                                ?: UiText.DynamicString("Set Privacy status error"),
+                            isLoading = false
+                        )
+                    }
+                }
+        }
+    }
+
+    fun onPushEnabledChanged(value: Boolean) {
+        viewModelScope.launch {
+            _uiState.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
+            userRepository.setPushEnabled(value)
+                .onSuccess {
+                    _uiState.update {
+                        it.copy(
+                            userProfile = it.userProfile?.copy(pushEnabled = value),
+                            isLoading = false
+                        )
+                    }
+                }.onFailure { e ->
+                    Log.e("SET_PUSH-MSG_ERROR", "Set push messages error: ${e.message}", e)
+                    _uiState.update {
+                        it.copy(
+                            error = e.localizedMessage?.let { msg -> UiText.DynamicString(msg) }
+                                ?: UiText.DynamicString("Set Push-messages error"),
+                            isLoading = false
+                        )
+                    }
+                }
+        }
+
+    }
+
+    fun onEmailEnabledChanged(value: Boolean) {
+        viewModelScope.launch {
+            _uiState.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
+            userRepository.setEmailEnabled(value)
+                .onSuccess {
+                    _uiState.update {
+                        it.copy(
+                            userProfile = it.userProfile?.copy(emailEnabled = value),
+                            isLoading = false
+                        )
+                    }
+                }.onFailure { e ->
+                    Log.e("SET_EMAIL-MSG_ERROR", "Set email msg error: ${e.message}", e)
+                    _uiState.update {
+                        it.copy(
+                            error = e.localizedMessage?.let { msg -> UiText.DynamicString(msg) }
+                                ?: UiText.DynamicString("Set email msg error"),
+                            isLoading = false
+                        )
+                    }
+                }
+        }
+    }
+
+    fun onSoundEnabledChanged(value: Boolean) {
+        viewModelScope.launch {
+            _uiState.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
+            userRepository.setSoundEnabled(value)
+                .onSuccess {
+                    _uiState.update {
+                        it.copy(
+                            userProfile = it.userProfile?.copy(soundEnabled = value),
+                            isLoading = false
+                        )
+                    }
+                }.onFailure { e ->
+                    Log.e("SET_SOUND_ERROR", "Set sound error: ${e.message}", e)
+                    _uiState.update {
+                        it.copy(
+                            error = e.localizedMessage?.let { msg -> UiText.DynamicString(msg) }
+                                ?: UiText.DynamicString("Set sound error"),
+                            isLoading = false
                         )
                     }
                 }
